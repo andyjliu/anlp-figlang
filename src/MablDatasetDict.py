8b42240cd43ed96b34f630365e27c63471899aba
shaily99 <shailyjbhatt99@gmail.com>
2023-11-13 17:21:27 -0500

    Added tokenization

--- src/MablDatasetDict.py
+++ src/MablDatasetDict.py
@@ -1,36 +1,66 @@
-from datasets import Dataset, DatasetDict
 import os
+from itertools import chain
+
+from datasets import Dataset, DatasetDict, load_dataset
 
 
 class MablDatasetDict(DatasetDict):
-
-    def __init__(self, data_dir='data'):
+    def __init__(self, data_dir="data"):
         super().__init__()
 
         self.dataset_dict = {}
-        for split in ['train', 'dev']:
+        data_files = {}
+        for split in ["train", "validation"]:
             file_path = f"{data_dir}/{split}/en.csv"
-            self.dataset_dict[split] = Dataset.from_csv(file_path)
+            # self.dataset_dict[split] = Dataset.from_csv(file_path)
+            self.dataset_dict[split] = load_dataset("csv", file_path)
 
         test_dir = f"{data_dir}/test"
         for file_name in os.listdir(test_dir):
             lang_short_name = file_name.split("_")[0]
             file_path = f"{test_dir}/{file_name}"
-            self.dataset_dict[f"test-{lang_short_name}"] = Dataset.from_csv(file_path)
+            # self.dataset_dict[f"test-{lang_short_name}"] = Dataset.from_csv(file_path)
+            self.dataset_dict[f"test-{lang_short_name}"] = load_dataset(
+                "csv", file_path
+            )
 
     def get_dataset_dict(self):
         return self.dataset_dict
 
     def tokenize(self, tokenizer):
         def tokenize_examples(examples):
-            # TODO - tokenize a batch of examples
-            return examples
+            first_sentences = [
+                [startphrase] * 2 for startphrase in examples["startphrase"]
+            ]
+            second_sentences = [
+                [examples[end][i] for end in ["ending1", "ending2"]]
+                for i in range(len(examples["startphrase"]))
+            ]
+            labels = examples["labels"]
+            first_sentences = sum(first_sentences, [])
+            second_sentences = sum(second_sentences, [])
 
-        for key in ['train', 'dev']:
+            # Tokenize
+            tokenized_examples = tokenizer(
+                first_sentences,
+                second_sentences,
+                max_length=128,
+                padding=False,
+                truncation=True,
+            )  # currently max_len and padding and truncation are hardcoded, but we can flagify them later.
+
+            tokenized_inputs = {
+                k: [v[i : i + 2] for i in range(0, len(v), 2)]
+                for k, v in tokenized_examples.items()
+            }
+            tokenized_inputs["labels"] = labels
+            return tokenized_inputs
+
+        for key in ["train", "validation"]:
             dataset = self.dataset_dict[key]
             tokenized_dataset = dataset.map(tokenize_examples, batched=True)
             self.dataset_dict[key] = tokenized_dataset
 
 
 if __name__ == "__main__":
-    print(MablDatasetDict(data_dir='../data').get_dataset_dict())
+    print(MablDatasetDict(data_dir="../data").get_dataset_dict())
--- src/Trainer.py
+++ src/Trainer.py
@@ -1,34 +1,48 @@
 import argparse
 import os
-from transformers import TrainingArguments, Trainer, AutoModelForMaskedLM, AutoTokenizer
 
 from src.MablDatasetDict import MablDatasetDict
+from transformers import (
+    AutoModelForMultipleChoice,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments,
+)
 
-HF_MODEL_NAME_XLMR = 'xlm-roberta-large'
+HF_MODEL_NAME_XLMR = "xlm-roberta-large"
+
 
 class MyTrainer:
-
-    def __init__(self, model, output_dir, batch_size, learning_rate, num_training_epochs, train_dataset, eval_dataset):
+    def __init__(
+        self,
+        model,
+        output_dir,
+        batch_size,
+        learning_rate,
+        num_training_epochs,
+        train_dataset,
+        eval_dataset,
+    ):
         training_args = TrainingArguments(
             output_dir=output_dir,
             overwrite_output_dir=True,
-            evaluation_strategy='epoch',
-            save_strategy='epoch',
+            evaluation_strategy="epoch",
+            save_strategy="epoch",
             per_device_train_batch_size=batch_size,
             per_device_eval_batch_size=batch_size,
             learning_rate=learning_rate,
             num_train_epochs=num_training_epochs,
             load_best_model_at_end=True,
-            metric_for_best_model='loss',
+            metric_for_best_model="loss",
             greater_is_better=False,
-            report_to=['wandb']
+            report_to=["wandb"],
         )
 
         self.trainer = Trainer(
             model=model,
             args=training_args,
             train_dataset=train_dataset,
-            eval_dataset=eval_dataset
+            eval_dataset=eval_dataset,
         )
 
     def train(self):
@@ -38,20 +52,20 @@ class MyTrainer:
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--model', type=str, default=HF_MODEL_NAME_XLMR)
-    parser.add_argument('--output-dir', type=str, default='output')
+    parser.add_argument("--model", type=str, default=HF_MODEL_NAME_XLMR)
+    parser.add_argument("--output-dir", type=str, default="output")
 
     return parser.parse_args()
 
 
 def get_model(model_name):
-    return AutoModelForMaskedLM.from_pretrained(model_name)
+    return AutoModelForMultipleChoice.from_pretrained(model_name)
 
 
 def configure_wandb():
-    os.environ['WANDB_PROJECT'] = 'anlp-figlang'
-    os.environ['WANDB_LOG_MODEL'] = 'true'
-    os.environ['WANDB_WATCH'] = 'false'
+    os.environ["WANDB_PROJECT"] = "anlp-figlang"
+    os.environ["WANDB_LOG_MODEL"] = "true"
+    os.environ["WANDB_WATCH"] = "false"
 
 
 if __name__ == "__main__":
@@ -59,7 +73,7 @@ if __name__ == "__main__":
 
     tokenizer = AutoTokenizer.from_pretrained(args.model)
 
-    mabl_dataset_dict = MablDatasetDict(data_dir='../data')
+    mabl_dataset_dict = MablDatasetDict(data_dir="../data")
     mabl_dataset_dict.tokenize(tokenizer)
     print(mabl_dataset_dict.get_dataset_dict())
 
@@ -69,11 +83,10 @@ if __name__ == "__main__":
         model=get_model(args.model),
         output_dir=args.output_dir,
         batch_size=32,
-        learning_rate=2e-5,
+        learning_rate=5e-6,
         num_training_epochs=20,
-        train_dataset=mabl_dataset_dict.get_dataset_dict()['train'],
-        eval_dataset=mabl_dataset_dict.get_dataset_dict()['dev']
+        train_dataset=mabl_dataset_dict.get_dataset_dict()["train"],
+        eval_dataset=mabl_dataset_dict.get_dataset_dict()["dev"],
     )
 
     trainer.train()
-
