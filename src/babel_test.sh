51db5a9b754dfc18fa24c4327495f77366d8efd0
shaily99 <shailyjbhatt99@gmail.com>
2023-11-16 17:50:36 -0500

    results et al

--- src/babel_test.sh
+++ src/babel_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=xlmr-su-inf
-#SBATCH --output=xlmr_su-inf.out
+#SBATCH --job-name=xlmr_zs_en_dev
+#SBATCH --output=xlmr_zs_en_dev.out
 #SBATCH --nodes=1
 #SBATCH --mem=48GB
 #SBATCH --time 0-03:00:00
@@ -15,8 +15,8 @@ source ~/.bashrc
 conda init bash
 conda activate figlang
 
-cp -r /data/tir/projects/tir5/users/shailyjb/anlp-figlang/su_50/checkpoint-960 /home/shailyjb/anlp-figlang/few_shot_trained_models/xlmr_su_50
-echo "Copied checkpoint"
+# cp -r /data/tir/projects/tir5/users/shailyjb/anlp-figlang/su_50/checkpoint-960 /home/shailyjb/anlp-figlang/few_shot_trained_models/xlmr_su_50
+# echo "Copied checkpoint"
 
-python /home/shailyjb/anlp-figlang/src/Inference.py --ckpt_path="/home/shailyjb/anlp-figlang/few_shot_trained_models/xlmr_su_50" --data_dir="/home/shailyjb/anlp-figlang/data/few_shot" --split="test_50" --output_dir="/home/shailyjb/anlp-figlang/few_shot_50_out/xlmr_su"
+python /home/shailyjb/anlp-figlang/src/Inference.py --ckpt_path="/home/shailyjb/anlp-figlang/xlmr_large_end_ckpt" --data_dir="/home/shailyjb/anlp-figlang/data" --split="validation" --output_dir="/home/shailyjb/anlp-figlang/xlmr_large_end_ckpt/test_out"
 echo "Done"
--- src/babel_train.sh
+++ src/babel_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=xlmr-yo
-#SBATCH --output=xlmr_yo.out
+#SBATCH --job-name=xlmr-jv
+#SBATCH --output=xlmr_jv.out
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:A6000:1
 #SBATCH --mem=48GB
@@ -15,9 +15,9 @@ source ~/.bashrc
 conda init bash
 conda activate figlang
 # python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/xlmr_large_defaults_with_logging" --train-file "/home/shailyjb/anlp-figlang/data/train/en.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
-# python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/hi_50" --train-file "/home/shailyjb/anlp-figlang/data/few_shot/train_merged/hi/hi_50.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
-# python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/id_50" --train-file "/home/shailyjb/anlp-figlang/data/few_shot/train_merged/id/id_50.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
-# python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/jv_50" --train-file "/home/shailyjb/anlp-figlang/data/few_shot/train_merged/jv/jv_50.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
+# python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/hi_50_re" --train-file "/home/shailyjb/anlp-figlang/data/few_shot/train_merged/hi/hi_50.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
+# python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/id_50_re" --train-file "/home/shailyjb/anlp-figlang/data/few_shot/train_merged/id/id_50.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
+python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/jv_50_re" --train-file "/home/shailyjb/anlp-figlang/data/few_shot/train_merged/jv/jv_50.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
 # python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/kn_50" --train-file "/home/shailyjb/anlp-figlang/data/few_shot/train_merged/kn/kn_50.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
 # python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/su_50" --train-file "/home/shailyjb/anlp-figlang/data/few_shot/train_merged/su/su_50.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
 # python /home/shailyjb/anlp-figlang/src/Trainer.py --output-dir="/data/tir/projects/tir5/users/shailyjb/anlp-figlang/sw_50" --train-file "/home/shailyjb/anlp-figlang/data/few_shot/train_merged/sw/sw_50.csv" --val-file "/home/shailyjb/anlp-figlang/data/validation/en.csv"
